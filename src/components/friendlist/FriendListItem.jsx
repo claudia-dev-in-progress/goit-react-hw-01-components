6723f3dd2d2960cf443b0fc24eb0db9246fa0e97
claudiapapp <pappileanaclaudia@gmail.com>
2023-08-13 18:11:40 +0300

    ===

--- src/components/friendlist/FriendListItem.jsx
+++ src/components/friendlist/FriendListItem.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export const FriendListItem = ({ avatar, name, isOnline }) => (
   <li class="list-item">
-    <div class={isOnline == true ? "online" : "offline"}></div>
+    <div class={isOnline === true ? "online" : "offline"}></div>
     <img class="avatar" src={avatar} alt="User avatar" width="48" />
     <p class="friend-name">{name}</p>
   </li>
